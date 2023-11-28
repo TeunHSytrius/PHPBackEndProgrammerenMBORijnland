2137abcccdc123b910d009b63725b74fa2599d7f
TeunHSytrius <TeunHSytrius@users.noreply.github.com>
2023-11-28 11:14:03 +0100

    Created array.php and made 3 assignments

---
+++ Opdrachten/arrays.php
@@ -0,0 +1,14 @@
+<?php
+//Opgave 5
+$product = [];
+$product[0] = "Boeken";
+$product[1] = "CD's";
+$product[2] = "Smartphones";
+$product[3] = "DVD's";
+
+//Opgave 6
+echo "<br>--- Opgave 6<br>";
+print_r($product);
+//Opgave 7
+echo "<br>";
+var_dump($product);
